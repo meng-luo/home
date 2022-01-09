70e353f35e20d864e38becc3569902b90bb17599
meng-luo <guorongdan1205@163.com>
2022-01-09 12:08:35 +0800

    新版主页

---
+++ source/js/footer.js
@@ -0,0 +1,9 @@
+document.write('<!-----------  footer----------->');
+document.write('' +
+    '<center>\n' +
+    ' <p style="color:#77c8ff;"><a style="color:#2798a0;" href="https://beian.miit.gov.cn" target="_blank">浙ICP备2021040035-1号</a> | ©2022 梦璃雨落</p>\n' +
+    '</center>\n' +
+    '');
+
+
+
---
+++ source/js/header.js
@@ -0,0 +1,13 @@
+document.write('<!-----------  公共js和css start----------->');
+document.write('' +
+    '<link rel="stylesheet" href="source/css/shubiao.css">\n' +
+    '<link rel="stylesheet" href="https://at.alicdn.com/t/font_2037988_gjhbcpzhs67.css" type="text/css">\n' +
+    '<link rel="apple-touch-icon" sizes="76x76" href="source/img/logo.png">\n' +
+    '<link rel="shortcut icon" href="source/img/logo.png" type="image/x-png">\n' +
+    '<link rel="stylesheet" href="source/css/mystyle.css">\n' +
+    '');
+
+
+
+
+
